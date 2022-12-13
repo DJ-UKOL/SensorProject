ebfb9f3ebb0e99f64de3a65f91a46248e55e7300
DJ-UKOL <dj-ukol@yandex.ru>
2022-12-13 19:50:03 +0500

    TASK-02 and add library xchart for graphics

--- SensorProject/src/main/java/ru/dinerik/SensorProject/Consumer.java
+++ SensorProject/src/main/java/ru/dinerik/SensorProject/Consumer.java
@@ -1,20 +1,25 @@
 package ru.dinerik.SensorProject;
 
-import org.springframework.http.HttpEntity;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.knowm.xchart.*;
+import org.knowm.xchart.style.Styler;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import ru.dinerik.SensorProject.models.Sensor;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
 
 public class Consumer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         RestTemplate restTemplate = new RestTemplate();
         // Post запрос
         // Регистрируем новый сенсор
-        Map<String, String> jsonToSend = new HashMap<>();
+/*        Map<String, String> jsonToSend = new HashMap<>();
         jsonToSend.put("name" , "Test Sensor From RestTemplate3");
 
         HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonToSend);
@@ -33,13 +38,44 @@ public class Consumer {
             String url2 = "http://localhost:8080/measurements/add";
             String response2 = restTemplate.postForObject(url2, request2, String.class);
             System.out.println(response2);
-        }
+        }*/
 
 
 
         // Получаем 1000 измерений с сервера
         String url3 = "http://localhost:8080/measurements";
-        String response3 = restTemplate.getForObject(url3, String.class);
-        System.out.println(response3);
+        ResponseEntity<String> response3 = restTemplate.getForEntity(url3, String.class);
+        //System.out.println(response3);
+
+
+        // Задание со звездочкой.
+        // Вывести график.
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response3.getBody());   // Преобразуем в дерево
+        Iterator<JsonNode> iterator = root.iterator();
+        List<Double> listDouble = new ArrayList<>();
+        List<Date> listDate = new ArrayList<>();
+        while (iterator.hasNext()) {
+            JsonNode jsonNode = iterator.next();
+            listDouble.add(jsonNode.get("value").doubleValue());
+            LocalDateTime localDateTime = LocalDateTime.parse(jsonNode.get("createdAt").asText());
+            Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+            Date date = Date.from(instant);
+            listDate.add(date);
+        }
+
+        //double[] xData = new double[]{0.0, 1.0, 2.0};
+        //double[] yData = new double[]{2.0, 1.0, 0.0};
+
+// Create Chart
+        //XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", listDouble, listDate);
+        final XYChart chart = new XYChartBuilder().width(1200).height(800).title("Area Chart").xAxisTitle("X").yAxisTitle("Y").build();
+        // Customize Chart
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNE);
+        chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Area);
+        chart.getStyler().setMarkerSize(8);
+        chart.addSeries("a", listDouble);
+        // Show it
+        new SwingWrapper(chart).displayChart();
     }
 }
